daceef16146fc123ab7642b43f257f85ac5cd99f
Rafael Fernandes <rafael.fernandes@webbrain.com.br>
2023-11-14 00:07:26 -0300

    autenticação de login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,29 +1,55 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import './styles.css';
+import { useNavigate } from 'react-router-dom';
 
+import './styles.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+import api from '../../service/api';
+
 import { ModalTermos, ModalPoliticaPrivacidade, ModalAlterarSenha } from '../../components/Modal/Modal';
 
 function LoginScreen() {
-    const [email, setEmail] = useState('')
-    const [senha, setSenha] = useState('')
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
     const [showModal, setShowModal] = useState(false);
     const [showPoliticaModal, setShowPoliticaModal] = useState(false);
     const [showAlterarSenhaModal, setShowAlterarSenhaModal] = useState(false);
 
+    const navigate = useNavigate();
+
+    async function login(e) {
+        e.preventDefault();
+
+        const data = {
+            userName,
+            password,
+        };
+
+        try {
+            const response = await api.post('auth/signin', data);
+
+            localStorage.setItem('userName', userName);
+            localStorage.setItem('accessToken', response.data.token);
+
+            navigate('/inicio');
+
+        } catch (error) {
+            alert('Falha no login, tente novamente')
+        }
+    }
+
     const usuarios =
-        [{ 'email': 'rafael@rafael', 'senha': 123 },
-        { 'email': 'will@will', 'senha': 123 }]
+        [{ 'userName': 'rafael@rafael', 'password': 123 },
+        { 'userName': 'will@will', 'password': 123 }]
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        const found = usuarios.find((usuario) => usuario.email === email)
-        console.log(found.email === email && found.senha === senha)
+        const found = usuarios.find((usuario) => usuario.userName === userName)
+        console.log(found.userName === userName && found.password === password)
     }
 
     function handleShowModal() {
@@ -71,33 +97,42 @@ function LoginScreen() {
                 </Col>
 
                 <Col sm="12" md="5" lg="7" className="d-flex align-items-center justify-content-center" >
-                    <form onSubmit={handleSubmit} className="form-login">
+                    <Col className="form-login">
+                        <form onSubmit={login} >
+                            <Col >
+                                <div className="mb-3">
+                                    <label htmlFor="exampleInputEmail1" className="form-label">E-mail:</label>
+                                    <input
+                                        type="userName"
+                                        className="form-control"
+                                        id="exampleInputEmail1"
+                                        aria-describedby="userName"
+                                        value={userName}
+                                        placeholder="exemplo@exemplo" onChange={(text) => { setUserName(text.target.value) }} />
+                                </div>
 
-                        <Col >
+                                <div className="mb-3">
+                                    <label htmlFor="password" className="form-label">Senha:</label>
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        id="password"
+                                        aria-describedby="password"
+                                        value={password}
+                                        onChange={(text) => { setPassword(text.target.value) }} />
+                                </div>
 
-                            <div className="mb-3">
-                                <label htmlFor="exampleInputEmail1" className="form-label">E-mail:</label>
-                                <input type="email" className="form-control" id="exampleInputEmail1"
-                                    aria-describedby="email" placeholder="exemplo@exemplo" onChange={(text) => { setEmail(text.target.value) }} />
-                            </div>
+                                <div className="mb-5">
+                                    <Link className="forgot-password" onClick={() => setShowAlterarSenhaModal(true)}>Esqueceu
+                                        sua senha?</Link>
+                                </div>
 
-                            <div className="mb-3">
-                                <label htmlFor="password" className="form-label">Senha:</label>
-                                <input type="password" className="form-control" id="password" aria-describedby="password" onChange={(text) => { setSenha(text.target.value) }} />
-                            </div>
-
-                            <div className="mb-5">
-                                <Link className="forgot-password" onClick={() => setShowAlterarSenhaModal(true)}>Esqueceu
-                                    sua senha?</Link>
-                            </div>
-
-                            <div className="d-grid">
-                                <button type="submit" className="btn btn-primary">Entrar</button>
-                            </div>
-                            <Link to="/inicio">TELA PRINCIPAL</Link>
-                        </Col>
-
-                        <Col className="mt-3">
+                                <div className="d-grid">
+                                    <button type="submit" className="btn btn-primary">Entrar</button>
+                                </div>
+                            </Col>
+                        </form>
+                        <Row className="mt-3">
 
                             <Col className="d-grid mb-3">
                                 <Link type="button" to="/cadastro-usuario" className="btn btn-dark" >Cadastrar-se</Link>
@@ -112,12 +147,11 @@ function LoginScreen() {
                                 <Col className="text-end">
                                     <Link onClick={() => setShowPoliticaModal(true)}>Política de privacidade</Link>
                                 </Col>
-
                             </Row>
-
-                        </Col>
-                    </form>
+                        </Row>
+                    </Col>
                 </Col>
+
             </Row>
 
             <ModalTermos showModal={showModal} onClose={handleCloseModal} />
